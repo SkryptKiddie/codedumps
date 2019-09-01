52981aca6de8f669c76f311ce12d1e8a5735c4b5
Joshek <joshua@joshek.pw>
2019-09-01 10:41:11 +0800

    Update MinecraftServerDeployer.sh

--- MinecraftServerDeployer.sh
+++ MinecraftServerDeployer.sh
@@ -4,5 +4,5 @@ cd minecraft
 wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
 java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
 sudo rm -rf eula.txt
-echo "eula=true" > myfile.txt
+echo "eula=true" > eula.txt.txt
 echo "Installed and ready!"
