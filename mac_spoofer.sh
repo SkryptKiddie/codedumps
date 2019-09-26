9accdabdf3a0c10b19724255998590d46f25db93
Joshek <joshua@joshek.pw>
2019-09-26 10:26:01 +0800

    Made the code better
    Added colour, better labels and more verbose

--- mac_spoofer.sh
+++ mac_spoofer.sh
@@ -1,19 +1,24 @@
-## change wlo1 to your appropriate interface
 #!/bin/bash
-echo Old MAC
-cat /sys/class/net/wlo1/address
-echo Stopping NetworkManager.service
+# Joshek's MAC (and IP on DCHP) changer/spoofer/generator
+IFACE="wlo1" ## input network interface here
+echo -e "\e[96mOld MAC address"
+ifconfig $IFACE | grep ether
+ifconfig $IFACE | grep inet
+echo
+echo -e "\e[91mStopping service" ## kill the NetworkManager.service
 sudo systemctl stop NetworkManager.service
+sudo ifconfig $IFACE down 
 echo Stopped service
-sudo ifconfig wlo1 down
-hexchars="0123456789ABCDEF"
+hexchars="0123456789ABCDEF" ## generate new mac address
 end=$( for i in {1..10} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
 MAC=00$end
-echo Setting new cloned MAC address
-sudo ifconfig wlo1 hw ether $MAC
-echo Done, bringing interface back online
-sudo ifconfig wlo1 up
+echo -e "\e[93mSetting new MAC address"
+sudo ifconfig $IFACE hw ether $MAC ## change cloned MAC
+echo -e "\e[92mDone, bringing $IFACE back online"
+sudo ifconfig $IFACE up ## start NetworkManager.service
 sudo systemctl start NetworkManager.service
-echo Service started, MAC changed
-echo New MAC
-cat /sys/class/net/wlo1/address
+echo Service started, MAC address changed
+echo
+echo -e "\e[96mNew MAC address"
+ifconfig $IFACE | grep ether
+## ifconfig $IFACE | grep inet ## not needed bc you won't be connected that quickly
